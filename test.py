59ab4067e796688c6543732628c2bed6bd419a42
Dahi <81472122+dahisea@users.noreply.github.com>
2024-01-08 00:37:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,11 @@
-# -*- coding: UTF-8 -*-
 import requests as req
-import json, sys, time
+import json, sys, time, random
 
-# Register an Azure app first; ensure the app has the following permissions:
-# Files: Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
-# User: User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
-# Mail: Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
-# After registration, be sure to click the button representing xxx to grant admin consent; otherwise, the Outlook API cannot be invoked.
-
-# Define the file path
 path = sys.path[0] + r'/temp.txt'
+num1 = 0
 
-# Define the function to get the token
 def gettoken(refresh_token):
-    # Define the request header
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    # Define the request parameters
     data = {
         'grant_type': 'refresh_token',
         'refresh_token': refresh_token,
@@ -23,27 +13,47 @@ def gettoken(refresh_token):
         'client_secret': secret,
         'redirect_uri': 'http://localhost:53682/'
     }
-    # Send a post request
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
-    # Parse the response result
     jsontxt = json.loads(html.text)
-    # Get the new token
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
-    # Write the new token to the file
-    with open(path, 'w+') as f:
-        f.write(refresh_token)
+    return access_token
 
-# Define the main function
 def main():
-    # Open the file
     fo = open(path, "r+")
-    # Read the file content
     refresh_token = fo.read()
-    # Close the file
     fo.close()
-    # Call the function to get the token
-    gettoken(refresh_token)
+    global num1
+    localtime = time.asctime(time.localtime(time.time()))
+    access_token = gettoken(refresh_token)
+    headers = {
+        'Authorization': access_token,
+        'Content-Type': 'application/json'
+    }
+    print('This run started at:', localtime)
+    urls = [
+        r'https://graph.microsoft.com/v1.0/me/drive/root',
+        r'https://graph.microsoft.com/v1.0/me/drive',
+        r'https://graph.microsoft.com/v1.0/drive/root',
+        r'https://graph.microsoft.com/v1.0/users',
+        r'https://graph.microsoft.com/v1.0/me/messages',
+        r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+        r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+        r'https://graph.microsoft.com/v1.0/me/drive/root/children',
+        r'https://api.powerbi.com/v1.0/myorg/apps',
+        r'https://graph.microsoft.com/v1.0/me/mailFolders',
+        r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
+    ]
+    for i, url in enumerate(urls):
+        try:
+            if req.get(url, headers=headers).status_code == 200:
+                num1 += 1
+                print(f"{i+1} call successful {num1} times")
+        except:
+            print("pass")
+            pass
 
-# Execute the main function
-main()
+for _ in range(6):
+    main()
+    for i in range(random.randint(100, 300), 0, -1):
+        time.sleep(1)
