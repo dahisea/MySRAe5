18484d8a46a846ea5ee6ffb5d37885819bc21fd6
Dahi <81472122+dahisea@users.noreply.github.com>
2024-01-08 00:50:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 # -*- coding: UTF-8 -*-
 import requests as req
-import json, sys, time, random
+import json
+import sys
+import time
+import random
 
-path = sys.path[0] + r'/temp.txt'
+# Register an Azure app with the following permissions:
+# files: Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
+# user: User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
+# mail: Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
+# Make sure to grant admin consent after registration for Outlook API to work
+
+# Define file path
+path = sys.path[0] + '/temp.txt'
+# Define successful call count
 num1 = 0
 
-def gettoken(refresh_token):
+# Define the function to get a token
+def get_token(refresh_token):
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     data = {
         'grant_type': 'refresh_token',
@@ -20,41 +32,43 @@ def gettoken(refresh_token):
     access_token = jsontxt['access_token']
     return access_token
 
+# Define the function to test API availability
 def main():
-    fo = open(path, "r+")
-    refresh_token = fo.read()
-    fo.close()
+    with open(path, "r+") as fo:
+        refresh_token = fo.read()
+
     global num1
     localtime = time.asctime(time.localtime(time.time()))
-    access_token = gettoken(refresh_token)
+    access_token = get_token(refresh_token)
     headers = {
         'Authorization': access_token,
         'Content-Type': 'application/json'
     }
-    print('此次运行开始时间为 :', localtime)
+    print('This run started at:', localtime)
     urls = [
-        r'https://graph.microsoft.com/v1.0/me/drive/root',
-        r'https://graph.microsoft.com/v1.0/me/drive',
-        r'https://graph.microsoft.com/v1.0/drive/root',
-        r'https://graph.microsoft.com/v1.0/users',
-        r'https://graph.microsoft.com/v1.0/me/messages',
-        r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
-        r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
-        r'https://graph.microsoft.com/v1.0/me/drive/root/children',
-        r'https://api.powerbi.com/v1.0/myorg/apps',
-        r'https://graph.microsoft.com/v1.0/me/mailFolders',
-        r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
+        'https://graph.microsoft.com/v1.0/me/drive/root',
+        'https://graph.microsoft.com/v1.0/me/drive',
+        'https://graph.microsoft.com/v1.0/drive/root',
+        'https://graph.microsoft.com/v1.0/users',
+        'https://graph.microsoft.com/v1.0/me/messages',
+        'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+        'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+        'https://graph.microsoft.com/v1.0/me/drive/root/children',
+        'https://api.powerbi.com/v1.0/myorg/apps',
+        'https://graph.microsoft.com/v1.0/me/mailFolders',
+        'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
     ]
+
     for i, url in enumerate(urls):
         try:
             if req.get(url, headers=headers).status_code == 200:
                 num1 += 1
-                print(f"{i+1}调用成功{num1}次")
+                print(f"{i+1} Call successful {num1} times")
         except:
             print("pass")
             pass
 
+# Execute 6 times
 for _ in range(6):
     main()
-    for i in range(random.randint(100, 300), 0, -1):
-        time.sleep(1)
+    time.sleep(random.randint(100, 300))
