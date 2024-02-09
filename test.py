de51f0c46be0d7428cd12f7fe356b760368eaef0
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-09 18:53:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,6 @@ import sys
 import time
 import random
 
-# Register an Azure app with the following permissions:
-# files: Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
-# user: User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
-# mail: Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
-# Make sure to grant admin consent after registration for Outlook API to work
 
 
 
@@ -82,3 +77,9 @@ def main():
 for _ in range(6):
     main()
     time.sleep(random.randint(100, 300))
+
+# Register an Azure app with the following permissions:
+# files: Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
+# user: User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
+# mail: Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
+# Make sure to grant admin consent after registration for Outlook API to work
