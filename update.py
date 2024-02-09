6c0a43f1d889e08e8ac4b51ebe780cbb252bb898
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-09 19:55:21 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,23 +1,13 @@
 import requests as req
 import json
 import sys
-import time
+import os
+import base64
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives import hashes
-
-
-
-
-
-
-
-
-
-
-
 
 # Define the file paths
 public_key_path = sys.path[0] + r'/public_key.pem'
@@ -32,11 +22,9 @@ def load_public_key():
     return public_key
 
 def encrypt_token(token, public_key):
-    hashed_token = hashes.Hash(hashes.SHA256(), backend=default_backend())
-    hashed_token.update(token.encode())
-    hashed_digest = hashed_token.finalize()
+    token = base64.b64encode(token.encode()) # Encode the token to base64
     cipher_text = public_key.encrypt(
-        hashed_digest,
+        token,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -44,6 +32,18 @@ def encrypt_token(token, public_key):
         )
     )
     return cipher_text
+
+def decrypt_token(cipher_text, public_key):
+    token = public_key.decrypt(
+        cipher_text,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    token = base64.b64decode(token).decode() # Decode the token from base64
+    return token
 
 def gettoken(refresh_token):
     try:
@@ -56,19 +56,19 @@ def gettoken(refresh_token):
             'redirect_uri': 'http://localhost:53682/'
         }
         html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
+        html.raise_for_status() # Raise an exception if the status code is not 200
         jsontxt = json.loads(html.text)
         refresh_token = jsontxt['refresh_token']
         access_token = jsontxt['access_token']
         public_key = load_public_key()
         encrypted_token = encrypt_token(refresh_token, public_key)
-        with open(token_path, 'wb') as f:
-            f.write(encrypted_token)
-    except Exception as e:
-        print("Error:", e)
+        with open(token_path, 'wb+') as f: # Open the file in binary write mode
+            f.truncate(0) # Clear the file content
+            f.write(encrypted_token) # Write the encrypted token
+    except (req.exceptions.HTTPError, json.decoder.JSONDecodeError) as e:
+        print("Error in requesting token:", e)
+    except (ValueError, TypeError) as e:
+        print("Error in encrypting token:", e)
 
-def main():
-    with open(token_path, "r+") as fo:
-        refresh_token = fo.read()
-    gettoken(refresh_token)
 
 main()
