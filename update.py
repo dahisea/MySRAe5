fde507fbea76af8b4273bda1f4e0ccb2bb25d5a6
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-09 18:29:39 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,23 +1,38 @@
-# -*- coding: UTF-8 -*-
 import requests as req
 import json
 import sys
 import time
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import hashes
 
-# Register an Azure app first, ensure the app has the following permissions:
-# files: Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
-# user: User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
-# mail: Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
-# After registration, be sure to click the button representing xxx to grant admin consent; otherwise, the Outlook API cannot be invoked.
+# Define the file paths
+public_key_path = sys.path[0] + r'/public_key.pem'
+token_path = sys.path[0] + r'/temp.txt'
 
-# Define the file path
-path = sys.path[0] + r'/temp.txt'
+def load_public_key():
+    with open(public_key_path, "rb") as key_file:
+        public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+    return public_key
 
-# Define the function to get the token
+def encrypt_token(token, public_key):
+    cipher_text = public_key.encrypt(
+        token.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return cipher_text
+
 def gettoken(refresh_token):
-    # Define the request header
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    # Define the request parameters
     data = {
         'grant_type': 'refresh_token',
         'refresh_token': refresh_token,
@@ -25,25 +40,18 @@ def gettoken(refresh_token):
         'client_secret': secret,
         'redirect_uri': 'http://localhost:53682/'
     }
-    # Send a post request
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
-    # Parse the response result
     jsontxt = json.loads(html.text)
-    # Get the new token
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
-    # Write the new token to the file
-    with open(path, 'w+') as f:
-        f.write(refresh_token)
+    public_key = load_public_key()
+    encrypted_token = encrypt_token(refresh_token, public_key)
+    with open(token_path, 'wb') as f:
+        f.write(encrypted_token)
 
-# Define the main function
 def main():
-    # Open the file
-    with open(path, "r+") as fo:
-        # Read the file content
+    with open(token_path, "r+") as fo:
         refresh_token = fo.read()
-    # Call the function to get the token
     gettoken(refresh_token)
 
-# Execute the main function
 main()
