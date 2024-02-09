e3276ce1a2fe143fc486c755e22d4784bd7ddba6
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-09 22:13:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 
 # Define file paths
-private_key_path = sys.path[0] + '/private_key.pem'
+private_key_path = sys.path[0] + '/private_key.txt'
 token_path = sys.path[0] + '/temp.txt'
 num1 = 0
 
