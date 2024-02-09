13c6b927a56f6469476ae54c9c65ed2039ab32d4
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-09 19:23:15 +0800

    Update update.py

--- update.py
+++ update.py
@@ -5,9 +5,8 @@ import time
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives import hashes
-
 
 
 
@@ -44,22 +43,25 @@ def encrypt_token(token, public_key):
     return cipher_text
 
 def gettoken(refresh_token):
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    data = {
-        'grant_type': 'refresh_token',
-        'refresh_token': refresh_token,
-        'client_id': id,
-        'client_secret': secret,
-        'redirect_uri': 'http://localhost:53682/'
-    }
-    html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
-    jsontxt = json.loads(html.text)
-    refresh_token = jsontxt['refresh_token']
-    access_token = jsontxt['access_token']
-    public_key = load_public_key()
-    encrypted_token = encrypt_token(refresh_token, public_key)
-    with open(token_path, 'wb') as f:
-        f.write(encrypted_token)
+    try:
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token,
+            'client_id': id,
+            'client_secret': secret,
+            'redirect_uri': 'http://localhost:53682/'
+        }
+        html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
+        jsontxt = json.loads(html.text)
+        refresh_token = jsontxt['refresh_token']
+        access_token = jsontxt['access_token']
+        public_key = load_public_key()
+        encrypted_token = encrypt_token(refresh_token, public_key)
+        with open(token_path, 'wb') as f:
+            f.write(encrypted_token)
+    except Exception as e:
+        print("Error:", e)
 
 def main():
     with open(token_path, "r+") as fo:
