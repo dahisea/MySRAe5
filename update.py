51bcb716b4173451d0b8052a4c22a7dcb5705e21
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-10 11:32:59 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,17 +1,7 @@
 import requests as req
 import json
 import sys
-import time
 import rsa
-
-
-
-
-
-
-
-
-
 
 
 
@@ -20,6 +10,10 @@ import rsa
 # Define the file paths
 path = sys.path[0] + '/temp.txt'
 public_key_path = sys.path[0] + '/public_key.txt'
+
+# Replace 'id' and 'secret' with your actual client ID and client secret
+id = 'your_client_id'
+secret = 'your_client_secret'
 
 # Define the function to get the token
 def gettoken(refresh_token):
@@ -36,16 +30,19 @@ def gettoken(refresh_token):
     # Send a post request
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
     # Parse the response result
-    jsontxt = json.loads(html.text)
-    # Get the new token
-    access_token = jsontxt['access_token']
-    # RSA encrypt the access_token
-    with open(public_key_path, "rb") as key_file:
-        public_key = rsa.PublicKey.load_pkcs1_openssl_pem(key_file.read())
-        encrypted_token = rsa.encrypt(access_token.encode(), public_key)
-    # Write the encrypted token to the file
-    with open(path, 'wb') as f:
-        f.write(encrypted_token)
+    jsontxt = html.json()
+    # Check if the response contains an access token
+    if 'access_token' in jsontxt:
+        access_token = jsontxt['access_token']
+        # RSA encrypt the access_token
+        with open(public_key_path, "rb") as key_file:
+            public_key = rsa.PublicKey.load_pkcs1_openssl_pem(key_file.read())
+            encrypted_token = rsa.encrypt(access_token.encode(), public_key)
+        # Write the encrypted token to the file
+        with open(path, 'wb') as f:
+            f.write(encrypted_token)
+    else:
+        print("Error: Access token not found in response.")
 
 # Define the main function
 def main():
