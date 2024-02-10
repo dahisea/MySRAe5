b8053e8355e2fd9457ebb953be060f534b8cd6a1
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-10 16:07:52 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,60 +1,30 @@
-# -*- coding: UTF-8 -*-
 import requests as req
 import json
 import sys
 import time
 
-
-
-
-
-
-
-
-
-
-
-
-
-# Register an Azure app first, ensure the app has the following permissions:
-# files: Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
-# user: User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
-# mail: Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
-# After registration, be sure to click the button representing xxx to grant admin consent; otherwise, the Outlook API cannot be invoked.
-
-# Define the file path
+CLIENT_SECRET = 'V378Q~w9Ox-guKNNF4Y0_zCjRIVwNwKbQLnzOduZ'
+CLIENT_ID = 'c9c9ab50-df3c-4837-a8cc-f0320a7d9214'
+TOKEN_URL = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
 path = sys.path[0] + r'/temp.txt'
 
-# Define the function to get the token
-def gettoken(refresh_token):
-    # Define the request header
+def get_refresh_token(client_id, client_secret):
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    # Define the request parameters
     data = {
-        'grant_type': 'refresh_token',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'redirect_uri': 'http://localhost:53682/'
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
     }
-    # Send a post request
-    html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
-    # Parse the response result
+    html = req.post(TOKEN_URL, data=data, headers=headers)
     jsontxt = json.loads(html.text)
-    # Get the new token
     refresh_token = jsontxt['refresh_token']
-    access_token = jsontxt['access_token']
-    # Write the new token to the file
-    with open(path, 'wb') as f:
-        f.write(refresh_token.encode('utf-8'))
+    return refresh_token
 
-# Define the main function
 def main():
-    # Open the file
-    with open(path, "rb") as fo:
-        # Read the file content
-        refresh_token = fo.read()
-    # Call the function to get the token
-    gettoken(refresh_token)
+    refresh_token = get_refresh_token(CLIENT_ID, CLIENT_SECRET)
+    with open(path, 'w') as f:
+        f.write(refresh_token)
+    print('Refresh token saved to temp.txt')
 
-# Execute the main function
-main()
+if __name__ == "__main__":
+    main()
