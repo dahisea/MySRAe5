af4b43e4122954e6e3e335e73e96a72502e0e6e0
Dahi <81472122+dahisea@users.noreply.github.com>
2024-02-10 19:31:45 +0800

    Update update.py

--- update.py
+++ update.py
@@ -3,6 +3,7 @@ import json
 import sys
 import time
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
